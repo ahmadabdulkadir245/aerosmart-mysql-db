0d717cf46676e9268986a52d403281bdd263e7f4
unknown <ahmadabdulkadir245@gmail.com>
2023-02-14 04:32:12 -0800

    added port in mysql database

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     database: 'node-complete',
-    password: 'Ahano1one$'
+    password: 'Ahano1one$',
+    port: 3306
 })
 
 module.exports = pool.promise()
