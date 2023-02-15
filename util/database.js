9a18f508587c479c6d5b93ba35ba80db9d7d88ed
unknown <ahmadabdulkadir245@gmail.com>
2023-02-15 14:15:55 -0800

    using environment variables

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: 'sql.freedb.tech',
-    user: 'freedb_aerosmart',
-    database: 'freedb_AerosmartDb',
-    password: 'ESe%vJ?Hu8Hbpyj',
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    database: process.env.SQL_DATABASE,
+    password: process.env.SQL_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
